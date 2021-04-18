5553db13aa74478027af2e0bb0de4fec23182c6d
Aakarshit Sharma <asdoon00@gmail.com>
2021-04-18 14:11:32 +0530

    update

--- power_insights/settings.py
+++ power_insights/settings.py
@@ -63,7 +63,9 @@ ROOT_URLCONF = 'power_insights.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
